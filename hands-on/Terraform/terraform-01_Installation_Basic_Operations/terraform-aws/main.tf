fc0497c1e1b46861920b95411eb772baf661be50
Hasan BALKANCI <swhsnblknc@gmail.com>
2022-07-24 21:32:50 +0200

    resources was added into main.tf

--- hands-on/Terraform/terraform-01_Installation_Basic_Operations/terraform-aws/main.tf
+++ hands-on/Terraform/terraform-01_Installation_Basic_Operations/terraform-aws/main.tf
@@ -11,3 +11,10 @@ provider "aws" {
   region = "us-east-1"
   # Configuration options
 }
+resource "aws_instance" "tf-ec2" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+} 
+  tags = {
+    Name = "created_by_tf"
+  }

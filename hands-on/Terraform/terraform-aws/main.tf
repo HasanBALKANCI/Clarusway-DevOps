c52a9b3d8cf1a7b542aeca4bfad9ca338331eee3
Hasan BALKANCI <swhsnblknc@gmail.com>
2022-07-26 14:25:03 +0200

    terraforms command was recapped

--- hands-on/Terraform/terraform-aws/main.tf
+++ hands-on/Terraform/terraform-aws/main.tf
@@ -22,17 +22,28 @@ provider "aws" {
 #   }
 # } 
 resource "aws_instance" "tf-ec2" {
-  ami           = "ami-0ed9277fb7eb570c9"
+  # ami           = "ami-0ed9277fb7eb570c9"
+  ami           = "ami-042e8287309f5df03"
   instance_type = "t2.micro"
   key_name      = "firstkey" # write your pem file without .pem extension>
   tags = {
-    "Name" = "tf-ec2"
+    # "Name" = "tf-ec2"
+    Name = "tf-ec2-ubuntu"
   }
 }
 resource "aws_s3_bucket" "bucket-tf" {
-  bucket = "hasan-1461-tf"
+  bucket = "hasan-1461-tf-new"
 
   tags = {
     Name = "hasan-1461-tf"
   }
+}
+output "tf_example_public_ip" {
+  value = aws_instance.tf-ec2.public_ip
+}
+output "tf_example_s3_meta" {
+  value = aws_s3_bucket.bucket-tf.region
+}
+output "tf_example_private_ip" {
+  value = aws_instance.tf-ec2.private_ip
 }

9c5014060feb7d1e8599c317cd6641a2abfcd72e
Hasan BALKANCI <swhsnblknc@gmail.com>
2022-07-27 15:11:30 +0200

    Docker handson_01 was added

--- hands-on/Terraform/terraform-aws/main.tf
+++ hands-on/Terraform/terraform-aws/main.tf
@@ -77,7 +77,7 @@ output "tf_example_s3_meta" {
 output "tf_example_private_ip" {
   value = aws_instance.tf-ec2.private_ip
 }
-output "variable-s3" {
-  value = aws_s3_bucket.tf-s3[*]
-}
+# output "variable-s3" {
+#   value = aws_s3_bucket.tf-s3[*]
+# }
 
